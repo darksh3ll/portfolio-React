130ce2d8db6ca4612fd448238b0166aaa22289b4
darksh3ll <darksh3ll.js@gmail.com>
2019-01-15 18:40:18 +0100

    fix bug

--- src/component/Apropos/Apropos.js
+++ src/component/Apropos/Apropos.js
@@ -5,18 +5,17 @@ const Apropos = () => {
     return (
         <main className="sectionMain mt-40 ">
             <p className="mainContent">
-                <span className="white">Bonjour je m'appelle stéphane</span>.
-                <p>Je poursuis actuellement une formation intensive de développeur web à la Wild Code School, un
-                    parcours extrêmement efficace qui permet d’acquérir le maximum de compétences .
-                </p>
+                <span className="white">Bonjour je m'appelle stéphane</span>.<br/>
 
-                <p>Futur diplômé d’un titre de
-                    Développeur web et mobile (Niveau III)
-                </p>
+                Je poursuis actuellement une formation intensive de développeur web à la Wild Code School, un
+                parcours extrêmement efficace qui permet d’acquérir le maximum de compétences .<br/>
 
-                <p>Je maîtrise les outils d'intégration HTML5/CSS3, et je suis en train de me spécialiser en JavaScript,
-                    React & Node.js.
-                </p>
+                Futur diplômé d’un titre de
+                Développeur web et mobile (Niveau III)<br/>
+
+                Je maîtrise les outils d'intégration HTML5/CSS3, et je suis en train de me spécialiser en JavaScript,
+                React & Node.js.
+
             </p>
             <a href="https://wildcodeschool.fr/" target="_blank" rel="noreferrer noopener">
                 <button>En savoir plus</button>
